c8d5b9f7f4f63c66027e77b131221da9834e8214
David Espinoza <despinoza1027@gmail.com>
2017-07-10 18:20:28 -0500

    completed exercise 02

---
+++ exercise_02.js
@@ -0,0 +1,15 @@
+var fib = [0, 1];
+
+var sum = 0;
+
+while (fib[0] + fib[1] < 4000000){
+    var i = fib[0] + fib[1];
+    fib[0] = fib[1];
+    fib[1] = i;
+
+    if (i % 2 === 0) {
+      sum += i;
+    }
+}
+
+console.log(sum);
